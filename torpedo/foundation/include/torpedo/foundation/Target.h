b9bd64fb807cf263fed6b3f1495ad123ee39e466
ndming <ndminh1101@gmail.com>
2025-04-21 10:18:27 +0200

    Treat layout transition from undefined the same as transfer src

--- torpedo/foundation/include/torpedo/foundation/Target.h
+++ torpedo/foundation/include/torpedo/foundation/Target.h
@@ -24,10 +24,15 @@ inline tpd::SyncPoint tpd::Target::getLayoutTransitionSrcSync(const vk::ImageLay
     using AccessMask    = vk::AccessFlagBits2;
 
     // Override to provide a more relax transition point, given that Target is designed mainly for compute write
+    // When a render Target is first used, it needs a layout transition from undefined, and for the rest of the program,
+    // the intended oldLayout to transition from will be general and transfer src. We keep it simple by treating layout
+    // transition from undefined the same as transfer src. This way, downstream won't have to perform an additional
+    // transition from undefined to transfer src and still keep the drawing logic consistent.
     using enum vk::ImageLayout;
     switch (oldLayout) {
-        case eGeneral: return { PipelineStage::eComputeShader, AccessMask::eShaderStorageWrite };
-        default:       return Image::getLayoutTransitionSrcSync(oldLayout);
+        case eUndefined: return Image::getLayoutTransitionSrcSync(eTransferSrcOptimal);
+        case eGeneral:   return { PipelineStage::eComputeShader, AccessMask::eShaderStorageWrite };
+        default:         return Image::getLayoutTransitionSrcSync(oldLayout);
     }
 }
 
